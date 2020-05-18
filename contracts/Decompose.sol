8eb978b92126f0eeca2be5b60cf92d6efd010ec0
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-05-17 23:35:43 -0700

    Construct contract with a rebalancing contract address as a parameter

--- contracts/Decompose.sol
+++ contracts/Decompose.sol
@@ -16,15 +16,18 @@ contract SetTokenInterface {
 
 
 contract Decompose {
-    address public rbAddress = 0x136faE4333EA36A24bb751E2d505D6ca4Fd9f00b;
-    RebalancingSetTokenInterface rbContract = RebalancingSetTokenInterface(
-        rbAddress
-    );
+    address public rbAddress;
+    RebalancingSetTokenInterface rbContract;
 
     address public setAddress;
     SetTokenInterface setContract;
 
     address public collateralAddress;
+
+    constructor(address _rbAddress) public {
+        rbAddress = _rbAddress;
+        rbContract = RebalancingSetTokenInterface(_rbAddress);
+    }
 
     function getComponents() public view returns (address[] memory) {
         return rbContract.getComponents();
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const Decompose = artifacts.require("Decompose");
 
 module.exports = function (deployer) {
-  deployer.deploy(Decompose);
+  deployer.deploy(Decompose, "0x136faE4333EA36A24bb751E2d505D6ca4Fd9f00b");
 };
