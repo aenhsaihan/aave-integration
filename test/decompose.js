c94b8bd4cd7a0bc9ccd0efe0b448c3af6973a6b9
Miguel Cervera <miguelcerverac@gmail.com>
2020-05-19 12:25:22 -0700

    test set with 2 components

--- test/decompose.js
+++ test/decompose.js
@@ -3,17 +3,29 @@ const truffleAssert = require('truffle-assertions');
 let decomposeInstance;
 const CUSDC = "0x39AA39c021dfbaE8faC545936693aC917d5E7563";
 const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+const WBTC = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
 
 contract("Decomposer", (accounts) => {
   before(async() => {
     decomposeInstance = await Decomposer.deployed();
   });
-  
+
   it("should get components of ETHRSIAPY contract", async () => {
     const {components, units} = await decomposeInstance.decomposeSet.call("0x136faE4333EA36A24bb751E2d505D6ca4Fd9f00b");
 
+    assert.equal(components.length, 1, "There must be only 1 component in this Set");
     assert.include([CUSDC, WETH], components[0]);
-    assert.isAbove(units[0].toNumber(), 0);
+    assert(units[0].gt(0));
+  });
+
+  it("should get components of BTCETH7525 contract", async () => {
+    const {components, units} = await decomposeInstance.decomposeSet.call("0xA35Fc5019C4dc509394Bd4d74591a0bF8852c195");
+
+    assert.equal(components.length, 2, "There must be 2 components in this Set");
+    assert.include(components, WETH, "wETH should be a component.");
+    assert.include(components, WBTC, "wBTC should be a component.");
+    assert(units[0].gt(0));
+    assert(units[1].gt(0));
   });
 
   it("should reject when address to decompose is not a Set", async () => {
