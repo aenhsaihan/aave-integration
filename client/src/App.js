a545eee2dafc86987b23cbca11f4a172ffdf6e03
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-05-23 07:24:01 -0700

    Use web3 on the frontend to call decomposeAndPriceSet() on the smart contract

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import Web3 from "web3";
 import "./App.css";
+import {
+  TOKEN_SETS_DECOMPOSER_ABI,
+  TOKEN_SETS_DECOMPOSER_ADDRESS,
+} from "./config";
 
 class App extends Component {
   componentWillMount() {
@@ -9,9 +13,30 @@ class App extends Component {
 
   async loadBlockchainData() {
     const web3 = new Web3("http://localhost:8545");
-    console.log(web3);
     const accounts = await web3.eth.getAccounts();
     this.setState({ account: accounts[0] });
+
+    const tokenSetsComposer = new web3.eth.Contract(
+      TOKEN_SETS_DECOMPOSER_ABI,
+      TOKEN_SETS_DECOMPOSER_ADDRESS
+    );
+    this.setState({ tokenSetsComposer });
+
+    console.log(tokenSetsComposer);
+
+    const ethersi6040address = "0x93E01899c10532d76C0E864537a1D26433dBbDdB";
+    const {
+      components,
+      units,
+      prices,
+      setPrice,
+    } = await tokenSetsComposer.methods
+      .decomposeAndPriceSet(ethersi6040address)
+      .call();
+    console.log(components);
+    console.log(units);
+    console.log(prices);
+    console.log(setPrice);
   }
 
   constructor(props) {
---
+++ client/src/config.js
@@ -0,0 +1,5 @@
+import TokenSetsComposer from "./artifacts/TokenSetsDecomposer.json";
+
+export const TOKEN_SETS_DECOMPOSER_ADDRESS =
+  "0x82ED63208a71B8E3b5E1f045285E2A69C248F56B";
+export const TOKEN_SETS_DECOMPOSER_ABI = TokenSetsComposer.abi;
