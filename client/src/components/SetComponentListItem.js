b2a0b0829de667388ea15941cc28bd153203b78c
Miguel Cervera <miguelcerverac@gmail.com>
2020-05-24 18:10:53 -0700

    setup with infura and deployed contract

--- client/src/components/SetComponentListItem.js
+++ client/src/components/SetComponentListItem.js
@@ -3,7 +3,7 @@ import React from "react";
 const SetComponentListItem = ({component}) =>
   <li>
     <strong>{component.units} <a href={component.url} target="_blank">{component.symbol}</a></strong>
-    &nbsp;with a price of <strong>{component.price} {component.symbol}/ETH</strong>
+    &nbsp;with a price of <strong>{component.price} ETH/{component.symbol}</strong>
   </li>
 
 export default SetComponentListItem;
--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,7 @@ import TokenSetsComposer from "./artifacts/TokenSetsDecomposer.json";
 import ERC20 from "./artifacts/ERC20.json";
 
 export const TOKEN_SETS_DECOMPOSER_ADDRESS =
-  "0x1B891D9B990cca419d85dA504B6172bebF0E66fF";
+  "0x41a1174a3fb55c042bca22d9c107d6a684dda3b0";
 export const TOKEN_SETS_DECOMPOSER_ABI = TokenSetsComposer.abi;
 export const ERC20_ABI = ERC20.abi;
-export const WEB3_URL = "http://localhost:8546";
+export const WEB3_URL = "https://mainnet.infura.io/v3/4014f2ed6fed43a18a38c52338558308";
